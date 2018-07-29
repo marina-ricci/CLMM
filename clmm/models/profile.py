4f70f3f0df9e5404b4d14dffcab0d0a087f1f688
Matthew Fong <mxf140930@utdallas.edu>
2018-07-29 12:59:06 -0400

    Added all the methods for Profile1D
    Added weak lensing profile methods

--- clmm/models/profile.py
+++ clmm/models/profile.py
@@ -1,6 +1,32 @@
 '''General profile class that inherits from Model'''
 
-class Profile(Model) :
+class Profile1D(Model) :
     ''' '''
-    def __init__(self,) :
+    def __init__(self, z_lens, mass_definition, cosmology, func, params=None) :
         pass
+    
+    def density_3d(self,r):
+        pass
+    
+    def surface_density(self, r):
+        pass
+    
+    def mean_surface_density(self, r):
+        pass
+    
+    def delta_sigma(self, r):
+        pass
+    
+    def convergence(self, r, z_source):
+        pass
+    
+    def mean_convergence(self, r, z_source):
+        pass
+    
+    def shear(self, r, z_source):
+        pass
+    
+    def reduced_shear(self, r, z_source):
+        pass
+    
+    
