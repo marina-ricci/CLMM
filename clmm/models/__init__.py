783cd4db9d3ac4366a901b501a849d67c51c8482
brandynlee <lee.brandyn@gmail.com>
2018-07-31 12:26:00 -0400

    remove comment

--- clmm/models/__init__.py
+++ clmm/models/__init__.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-"""Modules that build off of the Model superclass"""
-=======
 """Sub-package containing models defined as objects with potentially free parameters that can be fit. These are the things we might want to fit for sample over. For example, M-c relation is a model even if we choose to fix c.
 
 
 """
-
->>>>>>> 5d386a22d8c8f9e2e55fff8b9195c054c07a9c8c
 from .model import *
 from .parameter import *
 from .profile import *
