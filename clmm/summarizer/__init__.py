e092f0d1d4d735773f1c668df396a872dc2aaf89
Miyoung Choi <myoung8739@gmail.com>
2018-07-31 11:46:12 -0500

    added doctrings to the subpackage __init__.py

--- clmm/summarizer/__init__.py
+++ clmm/summarizer/__init__.py
@@ -1,1 +1,7 @@
+"""
+This subpackage is containing straightforward operations to data, for which we would not fit free parameters. This includes bin edges. 
+
+
+"""
+
 from .shear_azimuthal_averager import *
