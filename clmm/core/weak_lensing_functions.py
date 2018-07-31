73f205d2e270d5c9cf954b90a1edf8ef0f7a13ec
Matthew Fong <mxf140930@utdallas.edu>
2018-07-31 17:41:09 -0400

    fixed beta_function name and discrete mean functions

--- clmm/core/weak_lensing_functions.py
+++ clmm/core/weak_lensing_functions.py
@@ -18,9 +18,9 @@ def sigma_crit(d_a_lens, d_a_source, d_a_lens_source) :
     
     return constants.c.to('Mpc/s') * constants.c.to('Mpc/s') / \
         (4. * np.pi *  constants.G.to('Mpc3 / (Msun  s2)') * \
-         d_a_lens * beta_function(d_a_source, d_a_lens_source) )
+         d_a_lens * beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source) )
 
-def beta_function(d_a_source, d_a_lens_source) :
+def beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source) :
     """
     Schrabback2016 arXiv:1611.03866
     """
@@ -30,12 +30,14 @@ def beta_function(d_a_source, d_a_lens_source) :
     else:
         return d_a_lens_source / d_a_source 
 
-def beta_discrete_mean(d_a_lens, d_a_source, d_a_lens_source) :
+def beta_discrete_mean(d_a_lens, d_a_source, d_a_lens_source, weight) :
     """
     Arithmetic mean for discrete lensing efficiency <beta> for given source redshift or redshifts
+    Schrabback2016 arXiv:1611.03866
     
     Parameters
     ----------
+    weight : shape weight
 
     z_source : float or array-like of floats
         Source redshift or redshifts
@@ -43,22 +45,27 @@ def beta_discrete_mean(d_a_lens, d_a_source, d_a_lens_source) :
     """
 
     if np.iterable(d_a_source) and all(isinstance(d_a_s, float) for d_a_s in d_a_source) :
-        return np.mean(beta_function(d_a_source, d_a_lens_source))
+        betaxweight = beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source)*weight
+        return sum(betaxweight)/sum(weight)
+    
+        #return np.mean(beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source))
 
     elif isinstance(d_a_source, float) :
-        return beta_function(d_a_source, d_a_lens_source)
+        return beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source)
 
     else :
         raise TypeError("d_a_source must be float or array-like of floats ")
         
 
 
-def beta_square_discrete_mean(d_a_lens, d_a_source, d_a_lens_source) :
+def beta_square_discrete_mean(d_a_lens, d_a_source, d_a_lens_source, weight) :
     """
     Mean of the square of the discrete lensing efficiency <beta^2>
+    Schrabback2016 arXiv:1611.03866
     
     Parameters
     ----------
+    weight : shape weight
 
     z_source : float or array-like of floats
         Source redshift or redshifts
@@ -66,10 +73,12 @@ def beta_square_discrete_mean(d_a_lens, d_a_source, d_a_lens_source) :
     """
 
     if np.iterable(d_a_source) and all(isinstance(d_a_z, float) for d_a_z in d_a_source) :
-        return np.mean(beta_function(d_a_source, d_a_lens_source)**2.)
+        beta2xweight = (beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source)**2.)*weight
+        return sum(beta2xweight)/sum(weight)
+        #return np.mean(beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source)**2.)
 
     elif isinstance(d_a_source, float) :
-        return beta_function(d_a_source, d_a_lens_source)**2.
+        return beta_geometric_lensing_efficiency(d_a_source, d_a_lens_source)**2.
 
     else :
         raise TypeError("d_a_source must be float or array-like of floats ")
---
+++ clmm/models/cosmo_dependent_models/cosmology_model.py
@@ -0,0 +1,55 @@
+
+import astropy.cosmology as apycosmo
+from astropy import constants
+import numpy as np
+
+def get_cosmology( cosmology_str ) :
+    """
+    Currently needs to be implemented to parse for astropy cosmology options
+    
+    cosmology_str : str
+        Label of cosmology
+
+
+    """
+    print("getting cosmology %s, need to implement options from astropy"%cosmology_str)
+
+    return apycosmo.FlatLambdaCDM(H0=70, Om0=0.3)
+
+
+
+def angular_diameter_distance(redshift, cosmology_str):
+    cosmology = get_cosmology(cosmology_str)
+    return cosmology.angular_diameter_distance( redshift )
+
+def angular_diameter_distance_two_objects(z_lens, z_source, cosmology_str) :
+    """
+    Angular diameter distance between two objects
+    Hogg+00 Eqn (19)
+    
+    Parameters
+    ----------
+    
+    z_lens : float
+        Lens redshift or redshifts
+    
+    z_source : float
+        Source redshift or redshifts
+    
+    cosmology : str
+        Name of cosmology
+        
+    """
+    
+    
+    cosmology = get_cosmology(cosmology_str)
+    #Need cosmology to be defined else the code breaks when using cosmo.H(0).to...
+    Omega_k = 0 #for flat LCDM
+    hubble_radius= constants.c.to('Mpc/s')/cosmology.H0.to('/s')                  
+    ang_diameter_lens = cosmology.angular_diameter_distance(z_lens)
+    ang_diameter_source = cosmology.angular_diameter_distance(z_source)
+    return (ang_diameter_source*np.sqrt(1.+ Omega_k*ang_diameter_lens**2/hubble_radius**2) \
+            - ((1. + z_lens)/(1.+z_source))*ang_diameter_lens*np.sqrt(1.+ Omega_k*ang_diameter_source**2/hubble_radius**2))/(1.+z_source)
+
+
+
